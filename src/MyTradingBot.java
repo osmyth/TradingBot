ac3fc3a387b80e3eec3880c0f9baae1b694632e8
osmyth <osmyth@gmail.com>
2013-06-11 22:23:38 +0100

    commit

--- src/MyTradingBot.java
+++ src/MyTradingBot.java
@@ -6,16 +6,19 @@ import com.lmax.api.orderbook.OrderBookEvent;
 import com.lmax.api.orderbook.OrderBookEventListener;
 import com.lmax.api.orderbook.OrderBookSubscriptionRequest;
 
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class MyTradingBot implements LoginCallback, OrderBookEventListener {
     private Session session;
 
+    private Map<Long, FixedPointNumber> data;
+
     private final static long SPX500_INSTRUMENT_ID = 100093;
     private final static long EURUSD_INSTRUMENT_ID = 4001;
-
-    private FixedPointNumber lastHourHighValuationBidPrice;
-    private FixedPointNumber lastHourLowValuationBidPrice;
-    private long lastValuationTimeStamp;
 
     public static void main(String[] args) {
         MyTradingBot myTradingBot = new MyTradingBot();
@@ -24,39 +27,64 @@ public class MyTradingBot implements LoginCallback, OrderBookEventListener {
         lmaxApi.login(new LoginRequest("apetherapi", "testlmax1", ProductType.CFD_DEMO), myTradingBot);
     }
 
+    public MyTradingBot() {
+        data = new HashMap<Long, FixedPointNumber>();
+    }
+
+
     public void notify(OrderBookEvent orderBookEvent) {
 
         // Read the valuationBidPrice and timeStamp
         FixedPointNumber currentValuationBidPrice = orderBookEvent.getValuationBidPrice();
         long currentTimeStamp = orderBookEvent.getTimeStamp();
 
-        // First time this is called both the high and low will be null so set the current bid to both
-        if(lastHourLowValuationBidPrice == null && lastHourHighValuationBidPrice == null) {
-            lastHourLowValuationBidPrice = currentValuationBidPrice;
-            lastHourHighValuationBidPrice = currentValuationBidPrice;
-            lastValuationTimeStamp = currentTimeStamp;
-        }
+        long hourStartTime = currentTimeStamp-36000000;
+        long hourEndTime = currentTimeStamp;
 
-        // if an hour has elapsed...
-        if(currentTimeStamp > (lastValuationTimeStamp + 3600000)) {
+        FixedPointNumber lastHourLowValuationBidPrice = getValuationBidPrice(hourStartTime, hourEndTime, false);
+        FixedPointNumber lastHourHighValuationBidPrice = getValuationBidPrice(hourStartTime, hourEndTime, true);
+
+        if(lastHourLowValuationBidPrice != null || lastHourHighValuationBidPrice != null) {
+
+            System.out.println("Current Bid Price: "+currentValuationBidPrice+" Last Hour LOW: "+lastHourLowValuationBidPrice + ", HIGH: "+lastHourHighValuationBidPrice);
 
             // if the current is lower than the last low, set current to last low
             if(currentValuationBidPrice.longValue() < lastHourLowValuationBidPrice.longValue()) {
-                lastHourLowValuationBidPrice = currentValuationBidPrice;
+                System.out.println("Execute Trade - Found new LOW for Last hour: " + currentValuationBidPrice);
             }
 
             // if the current is higher than the last high, set current to last high
             if(currentValuationBidPrice.longValue() > lastHourHighValuationBidPrice.longValue()) {
-                lastHourHighValuationBidPrice = currentValuationBidPrice;
+                System.out.println("Execute Trade - Found new HIGH for Last hour: " + currentValuationBidPrice);
             }
 
-            // reset the lastTimeStamp
-            lastValuationTimeStamp = currentTimeStamp;
-
-            //System.out.println("TimeStamp: "+currentTimeStamp + ", valuationBidPrice: "+valuationBidPrice);
         }
 
-        //System.out.println("Market data: "+ orderBookEvent);
+        //System.out.println("Adding: "+new Date(currentTimeStamp)+": "+currentValuationBidPrice);
+        data.put(currentTimeStamp, currentValuationBidPrice);
+    }
+
+    private FixedPointNumber getValuationBidPrice(long startTime, long endTime, boolean lastHigh) {
+
+        FixedPointNumber lastHourValuationBidPrice = null;
+
+        for(Long key : data.keySet()) {
+            if(key < endTime && key >= startTime) {
+                FixedPointNumber value = data.get(key);
+                if(lastHourValuationBidPrice==null) {
+                    lastHourValuationBidPrice = value;
+                }
+
+                if(lastHigh && value.longValue() > lastHourValuationBidPrice.longValue()) {
+                    lastHourValuationBidPrice = value;
+                } else if(!lastHigh && value.longValue() < lastHourValuationBidPrice.longValue()) {
+                    lastHourValuationBidPrice = value;
+                }
+
+            }
+        }
+
+        return lastHourValuationBidPrice;
     }
 
     public void onLoginSuccess(Session session) {
